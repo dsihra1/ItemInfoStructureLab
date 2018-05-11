91c7e25784ad1eb12187b3e0d1930ab6546ae594
Devon Sihra <devonsihra12@outlook.com>
2018-05-10 23:02:27 -0400

    Lab files

---
+++ Lab07-StartingFiles/ItemInfoStructure/FrmItemInfo.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+//Devon Sihra
+namespace ItemInfoStructure
+{
+    public partial class FrmItemInfo : Form
+    {
+        public FrmItemInfo()
+        {
+            InitializeComponent();
+        }
+
+        List<Item> list = new List<Item>();
+
+        private void btnAdd_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                Item it = new Item();
+
+                it.ItemNumber = int.Parse(txtNumber.Text);
+                it.ItemPrice = double.Parse(txtPrice.Text);
+                it.ItemDescription = txtDescription.Text;
+                list.Add(it);
+
+
+                txtNumber.Focus();
+                txtDescription.Clear();
+                txtPrice.Clear();
+                txtNumber.Clear();
+            }
+            catch
+            {
+                MessageBox.Show("Custom Message");
+            }
+          
+
+        }
+
+        private void btnDisplay_Click(object sender, EventArgs e)
+        {
+            StringBuilder display = new StringBuilder("Items");
+
+            foreach(Item temp in list)
+            {
+               
+                display.Append("\n\n Items Number: " + temp.ItemNumber +
+                               "\n Item Price: " + temp.ItemPrice +
+                               "\nItem Decription: " + temp.ItemDescription);
+            }
+            MessageBox.Show(display.ToString());
+        }
+    }
+}
---
+++ Lab07-StartingFiles/ItemInfoStructure/Item.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace ItemInfoStructure
+{
+    public struct Item
+    {
+
+        public const string StoreName = "df";
+
+        private int number;
+
+        private double price;
+
+        private string description;
+
+        public int ItemNumber
+        {
+            get { return number; }
+
+            set
+            {
+                number = value;
+
+            }
+        }
+        public double ItemPrice
+        {
+            get { return price; }
+
+            set
+            {
+                   price = value;
+            }
+        }
+
+        public string ItemDescription
+        {
+            get { return description; }
+
+            set
+            {
+                    description = value;
+            }
+           
+           
+        }
+
+    }
+}
